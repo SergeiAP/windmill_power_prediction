29ceef3a5bac9e7cfbe5654604b656ae00bbc57a
Sergei Parshin <Sergei.A.P@yandex.com>
2022-05-27 18:20:32 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Science case for windmill power prediction based on weather. Based on Data Challenge of Air Liquide and TotalEnergies companies in 2021. The link of the competition is https://datascience.total.com/fr/challenge/19/details#.',
+    author='Sergei Parshin',
+    license='MIT',
+)
